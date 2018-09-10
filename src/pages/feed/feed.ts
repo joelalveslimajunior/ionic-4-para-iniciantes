a609fb7fc5bafa171c99e05145c4cff44bd2b6f2
Joel Alves <jr.alves18@gmail.com>
2018-09-10 00:08:16 -0300

    First commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  //public
+  //protected
+  //private
+  public nome_usuario:string = "Joel Alves";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros(num1:number, num2:number):void{
+    alert(num1+num2);
+  }
+  
+  ionViewDidLoad() {
+    //this.somaDoisNumeros(1, 10);
+  }
+
+}
